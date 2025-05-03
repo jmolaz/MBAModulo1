84a7036f873c87ac8812ba515700d4c3e4089f93
Jefferson Molaz <jefferson.molaz@gmail.com>
2025-05-03 06:57:27 -0300

    API e MVC na mesma base

--- MBAModulo1.Core/obj/Debug/net9.0/MBAModulo1.Core.AssemblyInfo.cs
+++ MBAModulo1.Core/obj/Debug/net9.0/MBAModulo1.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MBAModulo1.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+639a2a3d04cd8752d0bf523be0e60d04950f92a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fab67962ec1c1c90dd6e1e0c7d8962a16027ae80")]
 [assembly: System.Reflection.AssemblyProductAttribute("MBAModulo1.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MBAModulo1.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
