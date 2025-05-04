5cad504e6839f4283893c20ac8aa03e27c242760
Jefferson Molaz <jefferson.molaz@gmail.com>
2025-05-04 05:59:45 -0300

    Tela de Login

--- MBAModulo1.Core/obj/Debug/net9.0/MBAModulo1.Core.AssemblyInfo.cs
+++ MBAModulo1.Core/obj/Debug/net9.0/MBAModulo1.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MBAModulo1.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f9260c5ddf37d5886790ed324fc221534b5289b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4aed38ee878fd80d95327e0cee9c26d0e7201d78")]
 [assembly: System.Reflection.AssemblyProductAttribute("MBAModulo1.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MBAModulo1.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
